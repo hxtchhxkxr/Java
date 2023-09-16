a30345717eca73ba8fec75521ac28cdb3277c012
시은 <113548469+hxtchhxkxr@users.noreply.github.com>
2023-09-16 23:35:27 +0900

    Update Homework1.java

--- Homework1.java
+++ Homework1.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Homework1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("정수를 입력하세요: ");
+            int input = sc.nextInt();
+            sum += input;
+            System.out.println("현재까지 입력된 정수의 합은 " + sum + "입니다.");
+        }
+
+        sc.close();
+    }
 }
